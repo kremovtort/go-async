53b8d536a191219512c938fd82af4dc070d5e4ff
Alexander Makarov <i@kremovtort.ru>
2025-04-12 20:15:02 +0000

    don't use legacy field names in mkShell

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+          packages = with pkgs; [
             go
             gopls
             delve
